bbe1e9f5a6a1f14ea0459381acc980d403442b0d
androsh7@gmail.com <androsh7@gmail.com>
2026-01-24 13:17:30 -0700

    Updated script to use docker batch build

--- build.py
+++ build.py
@@ -1,81 +1,75 @@
 """Builds docker images"""
 
 # Standard libraries
+import os
 import subprocess
 from pathlib import Path
 from dataclasses import dataclass
 
+# Project libraries
+from config import (
+    LIBC_TO_DOCKERFILE,
+    ARCHITECTURES,
+    PYTHON_VERSIONS,
+    OPENSSL_VERSION,
+    NAMESPACE,
+    PARENT_DIRECTORY,
+)
+
 # Constants
 with open("VERSION.txt", "r", encoding="utf-8") as version_file:
     VERSION = version_file.read().strip()
-PARENT_DIRECTORY = Path(__file__).parent
 
-#---------------------------------------------------------------------------------#
-#================================= CONFIGURATION =================================#
-#---------------------------------------------------------------------------------#
-LIBC_TO_DOCKERFILE = [
-        (
-            "glibc-2.17",
-            PARENT_DIRECTORY / "docker_files" / "glibc-2_17-compiler-Dockerfile",
-        ),
-        (
-            "glibc-2.28",
-            PARENT_DIRECTORY / "docker_files" / "glibc-2_28-compiler-Dockerfile",
-        ),
-        (
-            "musl-1.2",
-            PARENT_DIRECTORY / "docker_files" / "musl-1_2-compiler-Dockerfile",
-        ),
-    ]
-ARCHITECTURES = ["x86_64", "aarch64"]
-PYTHON_VERSIONS = ["3.13.11", "3.14.2"]
-OPENSSL_VERSION = "3.0.18"
-#---------------------------------------------------------------------------------#
-#================================= CONFIGURATION =================================#
-#---------------------------------------------------------------------------------#
 
 @dataclass(frozen=True)
 class ImageSpec:
-    name: str
+    tag: str
     dockerfile: Path
     architecture: str | None = None
     build_args: dict[str, str] | None = None
 
 
+def build_docker_bake_file(build_images: list[ImageSpec]) -> Path:
+    """Creates a docker bake file based on the list of images
 
+    Args:
+        build_images: list of images to build
 
-def build_docker_image(image_spec: ImageSpec):
-    """Build a docker image based on the given ImageSpec"""
-    command = [
-        "docker",
-        "build",
-        "-f",
-        str(image_spec.dockerfile),
-        f"--label=VERSION={VERSION}",
-        "-t",
-        f"androsh7/{image_spec.name}:{VERSION}",
-        "-t",
-        f"androsh7/{image_spec.name}:latest",
-    ]
+    Returns:
+        Path to the generated docker bake file
+    """
+    bake_file_path = PARENT_DIRECTORY / "docker-bake.hcl"
+    with open(file=bake_file_path, mode="w", encoding="utf-8") as bake_file:
+        bake_file.write('group "default" {\n  targets = [')
+        for image_spec in build_images:
+            bake_file.write(f'"{image_spec.tag.replace(".", "-")}", ')
+        bake_file.write("]\n}\n\n")
 
-    # Add build arguments
-    if image_spec.build_args:
-        for arg_key, arg_value in image_spec.build_args.items():
-            command.append("--build-arg")
-            command.append(f"{arg_key}={arg_value}")
-            command.append(f"--label={arg_key}={arg_value}")
-
-    # Add working directory label
-    command.extend(["--push", str(image_spec.dockerfile.parent)])
-
-    # Execute the build command
-    print(f"Building image: {image_spec.name}\nRunning command: {' '.join(command)}")
-    subprocess.run(command, check=True)
+        for image_spec in build_images:
+            bake_file.write(f'target "{image_spec.tag.replace(".", "-")}" {{\n')
+            bake_file.write(
+                f'  dockerfile = "{image_spec.dockerfile}"\n'.replace("\\", "/")
+            )
+            bake_file.write(
+                f'  tags = ["{NAMESPACE}/nuitka-compiler:{VERSION}-{image_spec.tag}", "{NAMESPACE}/nuitka-compiler:latest-{image_spec.tag}"]\n'
+            )
+            bake_file.write(
+                f'  platforms = ["linux/{"arm64" if image_spec.architecture == "aarch64" else "amd64"}"]\n'
+            )
+            bake_file.write(
+                f'  context = "{str(PARENT_DIRECTORY).replace("\\", "/")}"\n'
+            )
+            if image_spec.build_args:
+                bake_file.write("  args = {\n")
+                for arg_key, arg_value in image_spec.build_args.items():
+                    bake_file.write(f'    {arg_key} = "{arg_value}"\n')
+                bake_file.write("  }\n")
+            bake_file.write("}\n\n")
+    return bake_file_path
 
 
 def main():
     """Build docker images"""
-    subprocess.run(["docker", "login"], check=True)
 
     # Generate image list
     BUILD_IMAGES = []
@@ -84,8 +78,9 @@ def main():
             for python_version in PYTHON_VERSIONS:
                 BUILD_IMAGES.append(
                     ImageSpec(
-                        name=f"nuitka-compiler-{architecture}-{libc}-python-{python_version.rsplit('.', 1)[0]}",
+                        tag=f"{architecture}-{libc}-py{python_version.rsplit('.', 1)[0]}",
                         dockerfile=dockerfile,
+                        architecture=architecture,
                         build_args={
                             "ARCHITECTURE": architecture,
                             "PYTHON_VERSION": python_version,
@@ -94,12 +89,17 @@ def main():
                     )
                 )
 
-    # Build each image
-    for image_spec in BUILD_IMAGES:
-        build_docker_image(image_spec)
+    # Generate docker-bake.hcl file
+    bake_file_path = build_docker_bake_file(BUILD_IMAGES)
 
-    # Logout from docker
-    subprocess.run(["docker", "logout"], check=True)
+    # Build and push images using docker bake
+    os.environ["BUILDX_BAKE_ENTITLEMENTS_FS"] = "0"
+    subprocess.run(
+        ["docker", "bake", "--file", str(bake_file_path), "--push"], check=True
+    )
+
+    # Remove the bake file
+    os.remove(bake_file_path)
 
 
 if __name__ == "__main__":
---
+++ config.py
@@ -0,0 +1,30 @@
+"""Configuration for building images"""
+
+# Standard libraries
+from pathlib import Path
+
+# --------------------------------------------------------------------------------- #
+# ================================= CONFIGURATION ================================= #
+# --------------------------------------------------------------------------------- #
+PARENT_DIRECTORY = Path(__file__).parent
+LIBC_TO_DOCKERFILE = [
+    (
+        "glibc-2.17",
+        PARENT_DIRECTORY / "docker_files" / "glibc-2_17-compiler-Dockerfile",
+    ),
+    (
+        "glibc-2.28",
+        PARENT_DIRECTORY / "docker_files" / "glibc-2_28-compiler-Dockerfile",
+    ),
+    (
+        "musl-1.2",
+        PARENT_DIRECTORY / "docker_files" / "musl-1_2-compiler-Dockerfile",
+    ),
+]
+ARCHITECTURES = ["x86_64"]
+PYTHON_VERSIONS = ["3.9.24", "3.10.19", "3.11.14", "3.12.12", "3.13.11", "3.14.2"]
+OPENSSL_VERSION = "3.0.18"
+NAMESPACE = "androsh7"
+# --------------------------------------------------------------------------------- #
+# ================================= CONFIGURATION ================================= #
+# --------------------------------------------------------------------------------- #
