a53f3a6aaa0106c504fb7910723c4dcf713ada83
androsh7@gmail.com <androsh7@gmail.com>
2026-01-25 23:45:25 -0700

    Disabled arm architecture due to build issues

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ LIBC_TO_DOCKERFILE = [
         PARENT_DIRECTORY / "docker_files" / "musl-1_2-compiler-Dockerfile",
     ),
 ]
-ARCHITECTURES = ["x86_64", "aarch64"]
+ARCHITECTURES = ["x86_64"]
 PYTHON_VERSIONS = ["3.9.24", "3.10.19", "3.11.14", "3.12.12", "3.13.11", "3.14.2"]
 OPENSSL_VERSION = "3.0.18"
 NAMESPACE = "androsh7"
 SOURCE_URL = "https://github.com/Androsh7/nuitka_compiler_images"
-PARALLELISM = 1
+PARALLELISM = 4
 # --------------------------------------------------------------------------------- #
 # ================================= CONFIGURATION ================================= #
 # --------------------------------------------------------------------------------- #
